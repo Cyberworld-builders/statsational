e871bd0c65f9d49eac30572cf004fa02d55d6631
jay <jaylong255@gmail.com>
2018-04-27 08:15:42 -0500

    adding favicon and title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" href="{{ asset('/images/favicon.png') }}">
     <title>{{ config('app.name', 'Statsational') }}</title>
 
     <!-- Scripts -->
