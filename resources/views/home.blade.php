39cfe534866eae101e51116e2852b92b1c547514
jay <jaylong255@gmail.com>
2018-05-07 10:20:18 -0500

    changing auctions to table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,15 +14,28 @@
                         </div>
                     @endif
 
-                    <ul>
-                      @foreach($auctions as $auction)
-                        <li>
-                          <a class="dropdown-item" href="/auction/{{ $auction->id }}" >
-                              {{ __( $auction->name ) }}
-                          </a>
-                        </li>
-                      @endforeach
-                    </ul>
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th scope="col">Id</th>
+                          <th scope="col">Auction</th>
+                          <th scope="col">Start Time</th>
+                          <th scope="col">Owner</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+
+                        @foreach($auctions as $auction)
+                            <tr>
+                              <th scope="row">{{ $auction->id }}</th>
+                              <td><a class="dropdown-item" href="/auction/{{ $auction->id }}" >{{ __( $auction->name ) }}</a></td>
+                              <td>{{ $auction->start_time }}</td>
+                              <td>{{ $auction->user->name }}</td>
+                            </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
