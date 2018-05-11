b7e00a19bbb8eddb0a8748b59cf92b704126d2c9
jay <jaylong255@gmail.com>
2018-05-11 16:39:41 -0500

    implementing basic chat functinality into auction

--- resources/assets/js/components/messages/ChatForm.vue
+++ resources/assets/js/components/messages/ChatForm.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="input-group">
-        <input id="btn-input" type="text" name="message" class="form-control input-sm" placeholder="Type your message here..." v-model="newMessage" @keyup.enter="sendMessage">
-
-        <span class="input-group-btn">
-            <button class="btn btn-primary btn-sm" id="btn-chat" @click="sendMessage">
-                Send
-            </button>
-        </span>
+        <div class="col-sm-12 col-md-10 mt-1">
+          <input id="btn-input" class="form-control" placeholder="Type a message..."  v-model="newMessage" @keyup.enter="sendMessage" />
+        </div>
+        <div class="col-sm-3 col-md-2 pull-right text-right mt-1">
+          <button class="btn btn-primary " id="btn-chat" @click="sendMessage" >Send</button>
+        </div>
     </div>
 </template>
 
