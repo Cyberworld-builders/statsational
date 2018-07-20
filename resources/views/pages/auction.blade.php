861e27edda127af9600d4bbe5276d2ee8cf9c7eb
jay <jaylong255@gmail.com>
2018-07-20 13:04:10 -0500

    more responsive changes

--- resources/views/pages/auction.blade.php
+++ resources/views/pages/auction.blade.php
@@ -8,102 +8,133 @@
 
 
   <div class="auction">   <!-- the entire auction room -->
+
+
     <nav class="top-bar"> <!-- top bar nav -->
       <div class="row">
-      	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-8">
-      		Draft Room
-          <button class="btn "><span id="timer">@{{ timer }}</span></button>
-          Status: In Progress
-      	</div>
-      	<div v-if="showOwnerControls" class="owner-tools col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-2">
-          <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-                <a id="owner-tools" class="nav-link dropdown-toggle" href="/auctions" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    Commissioner
-                </a>
-                <div class="dropdown-menu" aria-labelledby="owner-tools">
-                  <a class="dropdown-item" href="#" @click="startNextItem">Start Next Item</a>
-                  <a class="dropdown-item" href="#" @click="resetTimer(auction.bid_timer)">Restart Clock</a>
-                  <a class="dropdown-item" href="#" @click="undoLastBid">Undo Last Bid</a>
-                  <a class="dropdown-item" href="#" >End Auction</a>
-                  <a class="dropdown-item" href="#" >Reload App</a>
-                  <a class="dropdown-item" href="#" @click="showModal"><i class="fa fa-plus"></i> Add Item</a>
-                </div>
-            </li>
-          </ul>
-      	</div>
-        <div v-if="showOwnerControls" class="owner-tools col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-2">
-          <div class="row icons">
-          	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-3">
-          		<a href="#"><span><i class="fa fa-bell"></i></span></a>
-          	</div>
-          	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-3">
-          		<a href="#"><span><i class="fa fa-flag"></i></span></a>
-          	</div>
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-3">
-          		<a href="#"><span><i class="fa fa-power-off"></i></span></a>
-          	</div>
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-3">
-          		<a href="#"><span><i class="fa fa-cog"></i></span></a>
-          	</div>
-          </div>
-      	</div>
-      </div>
-    </nav> <!-- end top bar nav -->
-    <nav class="bidding navbar-dash navbar-expand-md navbar-light"> <!-- bidding controls bar     -->
-      <div class="row">
-        <div class="navbar-home col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-2">
-          <a class="navbar-brand" href="/home">
-              <img class="logo-small" src="/images/logo.png" />
-          </a>
-        </div>
-        <div class="bidding-controls col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-5">
+      	<div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
           <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-              <p>
-                On the Block:&nbsp&nbsp&nbsp
-                <span>@{{ auction.item.name }}</span>
-              </p>
+            <div class="col-sm-2 col-md-3 col-lg-3 col-xl-2">
+              Pool: @{{ auction.name }} ($ @{{ Math.round(auction.item.bids[0].amount) }})
             </div>
-
-            <div id="current_bid" class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-              <p v-if="auction.item.bids.length" >
-                Current Bid:&nbsp&nbsp&nbsp <span>$ @{{ Number(auction.item.bids[0].amount) }}</span>
-              </p>
-              <p v-else>No bids yet.</p>
+            <div class="d-md-none d-lg-none d-xl-block col-xl-2">
+              On the Block: @{{ auction.item.name }}
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+            <div class="col-md-5 col-lg-3 col-xl-2">
               <p v-if="auction.item.bids.length > 0 && bidders[auction.item.bids[0].user_id]" id="hightest_bidder">
-                Highest Bidder:&nbsp&nbsp&nbsp <span>@{{ bidders[auction.item.bids[0].user_id].name }}</span>
+                Highest Bidder:
+                @{{ bidders[auction.item.bids[0].user_id].name }} ($ @{{  Math.round(auction.item.bids[0].amount) }})
               </p>
             </div>
+            <div class="d-none d-xl-block col-xl-2">
+              Time Remaining: @{{ timer }}
+            </div>
 
+            <div class="d-md-none col-lg-3  d-xl-block col-xl-2">
+                <span v-if="bidders[user.id]">Logged In: @{{ bidders[user.id].name }} ($ @{{ bidders[user.id].spend }})</span>
+            </div>
+
+            <div class="owner-tools col-sm-3 col-md-4 col-lg-3 col-xl-2">
+              <div class="row icons">
+                <div v-if="showOwnerControls" class="col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                  <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a id="owner-tools" class="nav-link dropdown-toggle text-right pull-right" href="/auctions" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <i class="fa fa-cog"></i>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="owner-tools">
+                          <a class="dropdown-item" href="#" @click="startNextItem">Start Next Item</a>
+                          <a class="dropdown-item" href="#" @click="resetTimer(auction.bid_timer)">Restart Clock</a>
+                          <a class="dropdown-item" href="#" @click="undoLastBid">Undo Last Bid</a>
+                          <a class="dropdown-item" href="#" >End Auction</a>
+                          <a class="dropdown-item" href="#" >Reload App</a>
+                          <a class="dropdown-item" href="#" @click="showModal"><i class="fa fa-plus"></i> Add Item</a>
+                        </div>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                  <a href="#"><span><i class="fa fa-flag"></i></span></a>
+                </div>
+                <div class="col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                  <a href="#"><span><i class="fa fa-bell"></i></span></a>
+                </div>
+                <div class="col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                  <a href="#"><span><i class="fa fa-power-off"></i></span></a>
+                </div>
+              </div>
+            </div>
           </div>
+      	</div>
+      </div> <!-- end of row -->
+    </nav> <!-- end top bar nav -->
+
+
+
+    <nav class="bidding navbar-dash navbar-expand-md navbar-light text-center"> <!-- bidding controls bar     -->
+      <div class="row">
+
+        <div class="logo-container d-none d-xl-block col-xl-2 ">
+          <a class="navbar-brand" href="/home">
+              <img class="logo-large" src="/images/logo.png" />
+          </a>
+      	</div>
+
+        <div class="logo-container col-md-1 col-lg-1 col-lg-block d-xl-none">
+          <a class="" href="/home">
+              <img class="logo-small" src="/images/footer_logo.png" />
+          </a>
+      	</div>
+
+        <div class="col-sm-1 col-md-1 col-lg-1 col-xl-1 ">
+          <h1 id="timer">@{{ timer }}</h1>
         </div>
-        <div class="bidding-controls controls col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-5">
+
+
+        <div class="bidding-controls controls col-sm-12 col-md-4 col-lg-4 col-xl-3">
           <div v-if="auction.queue && auction.queue.length">
-
-            		<button @click="lowerBid" class="btn bid-button"><i class="fa fa-minus"></i></button>
-
-                <input  @focus="$event.target.select()" id="manualBid" type="number" value="bid_amount" v-model="bid_amount">
-                  <b-alert class="minimum-bid-warning" variant="danger"
-                           dismissible
-                           :show="showMinimumBidWarning"
-                           @dismissed="showMinimumBidWarning=false">
-                  Your bid cannot be lower than the minimum bid!
-                  </b-alert>
-
-            		<button @click="raiseBid" class="btn bid-button"><i class="fa fa-plus"></i></button>
-                <button id="bidButton" @click="bid" class="btn bid-button">Bid</button>
+        		<button @click="lowerBid" class="btn bid-button"><i class="fa fa-minus"></i></button>
+            <input  @focus="$event.target.select()" id="manualBid" type="number" value="bid_amount" v-model="bid_amount">
+              <b-alert class="minimum-bid-warning" variant="danger"
+                       dismissible
+                       :show="showMinimumBidWarning"
+                       @dismissed="showMinimumBidWarning=false">
+              Your bid cannot be lower than the minimum bid!
+              </b-alert>
+            <button id="bidButton" @click="bid" class="btn bid-button">Bid</button>
+        		<button @click="raiseBid" class="btn bid-button"><i class="fa fa-plus"></i></button>
 
           </div>
+        </div>
 
 
+        <div class="bidding-controls d-md-block col-md-3 col-lg-3 col-xl-3">
+
+            <h1 v-if="auction.item.bids.length > 0 && bidders[auction.item.bids[0].user_id]">
+              @{{ auction.item.name }}
+            </h1>
+            <h1 v-else class="blinking">Waiting... </h1>
 
         </div>
+
+        <div class="bidding-controls d-md-none d-lg-none d-xl-block col-xl-3">
+          <h2 v-if="auction.item.bids.length > 0 && bidders[auction.item.bids[0].user_id]">
+            @{{ bidders[auction.item.bids[0].user_id].name }}
+            ($@{{ Math.round(auction.item.bids[0].amount) }})
+          </h2>
+          <h2 v-else>No bids yet.</h2>
+        </div>
+
+
+
+
 
       </div>
     </nav> <!-- end bidding controls bar -->
+
+
+
+
     <div class="row justify-content-center"> <!-- the main content area     -->
       <div class="col-md-12 auction-room"> <!-- full width container for the auction room    -->
 
@@ -159,7 +190,7 @@
                      </ul>
                    </div>
                  </div>
-                 <div class="col-xs-3 col-md-2">
+                 <div class="col-md-2">
                    <b-modal ref="myModalRef" hide-footer title="Add Item">
                      <form id="addItem" @submit.prevent="addItem" role="form">
                        <div class="form-group">
@@ -269,14 +300,11 @@
                   @dismissed="showBiddersOverviewDetail=false">
                   <div class=" ">
                     <div class="row">
-                      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
-                        <h3>Owned</h3>
+
+                      <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                        @{{ selectedBidder.name }}
                       </div>
-                      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                        <input class="form-control" v-model="selectedBidder.name" disabled />
-                      </div>
-                      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
-                      </div>
+
 
                     </div>
                     <div class="clearfix">
@@ -308,29 +336,22 @@
 
                <div class="bidders-overview container">
                  <div class="row">
-                   <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                   <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
                      <h3>Bidders Overview</h3>
                    </div>
-                   <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 text-right money-spent">
-                     <span>Money Spent ($)</span> &nbsp
-                     <span v-if="bidders[user.id]" class="spent">@{{ bidders[user.id].spend }}</span>
-                     <span v-else>0</span>
-                   </div>
+
                  </div>
                    <table class="table">
                      <thead>
                        <tr class="bidder-card ">
                          <th scope="col">Name</th>
-                         <th scope="col" class="text-right">Bal</th>
-                         <th scope="col" class="text-right">Avg. Spend</th>
-                         <th scope="col" class="text-right">Owned</th>
+                         <th scope="col" class="text-right">Spent</th>
+
                        </tr>
                        <tbody>
-                         <tr v-for="(bidder,index) in sorted_bidders"  @click="updateSelectedBidder(bidder.id)" class="bidder-card ">
-                           <td><strong>@{{ bidder.name }} </strong></td>
+                         <tr v-bind:class="[(bidder.id == user.id) ? 'isUser' : '']" v-for="(bidder,index) in sorted_bidders"  @click="updateSelectedBidder(bidder.id)" class="bidder-card ">
+                           <td ><strong>@{{ bidder.name }} </strong></td>
                            <td class="text-right"><span>$@{{ bidder.spend }}</span></td>
-                           <td class="text-right"><span>$@{{ bidder.average_spend }}</span></td>
-                           <td class="text-right"><span class="players-needed" v-if="bidder.item_count > 0"> @{{ bidder.item_count }}</span></td>
                          </tr>
                        </tbody>
                      </thead>
